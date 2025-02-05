4b82cfd4d66acc019058f804c90f93e6ae587ecf
Jayasurya-S <jayasurya4060@gmail.com>
2025-02-05 18:54:27 +0530

    first  commit

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const connectDB = require("./config/db");
 const cors = require("cors");
 const recipeRoutes = require("./routes/recipeRoutes");
 require("dotenv").config();
+const render_url = process.env.Node_server;
 
 const app = express();
 
@@ -12,6 +13,6 @@ app.use(cors());
 app.use(express.json());
 app.use("/api", recipeRoutes);
 
-app.listen(process.env.Node_server, () => {
+app.listen(render_url, () => {
   console.log("Server running on render");
 });
